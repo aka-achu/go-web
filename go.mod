a82032e96276596c1f8c0d1f69a56307e416a761
Achyuta Das <aka.achu.1612@gmail.com>
2020-09-18 22:24:36 +0530

    add: request tracing using context store

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/aka-achu/eidos v0.0.0-20200812134738-6fe045bca396
+	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.7.4
 	github.com/rs/cors v1.7.0
 	github.com/stretchr/testify v1.6.1 // indirect
--- middleware/no_auth.go
+++ middleware/no_auth.go
@@ -1,14 +1,22 @@
 package middleware
 
 import (
+	"context"
 	"github.com/aka-achu/go-web/logging"
+	"github.com/google/uuid"
 	"net/http"
 )
 
 func NoAuthLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logging.RequestLogger.Info(r.RemoteAddr, " ", r.Proto, " ", r.Method, " ", r.RequestURI)
-		next.ServeHTTP(w, r)
-
+		traceID := uuid.New().String()
+		logging.RequestLogger.Infof("TraceID-%s Addr-%s Protocol-%s Method-%s URI-%s",
+			traceID, r.RemoteAddr, r.Proto, r.Method, r.RequestURI)
+		next.ServeHTTP(
+			w,
+			r.WithContext(
+				context.WithValue(r.Context(), "trace_id", traceID),
+			),
+		)
 	})
 }
